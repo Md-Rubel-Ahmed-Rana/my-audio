9bb6d62ea985bc3c4943f980ff7c0672f5e05a2c
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-18 15:36:18 +0600

    features added

--- main.js
+++ main.js
@@ -11,11 +11,13 @@ const loadAudios = () => {
 const displayAudios = (audios) => {
     const totalMusic = document.getElementById("total-music");
     totalMusic.innerText = audios.length;
+    const total = document.getElementById("total");
+    total.innerText = audios.length;
     const musicContainer = document.getElementById("music-list");
     audios.forEach(audio => {
         const li = document.createElement("li");
         li.innerHTML = `
-        <span>${audio.id}. </span> <span> ${audio.fileName.slice(0, 25)}</span>
+        <a href="#"><span>${audio.id}. </span> <span> ${audio.fileName.slice(0, 25)}</span></a>
         `;
         musicContainer.appendChild(li);
 
@@ -24,6 +26,8 @@ const displayAudios = (audios) => {
 
 const showAudios = (audios) => {
     let fileNumber = 0;
+    const playing = document.getElementById("playing");
+    playing.innerText = fileNumber + 1;
     const nextBtn = document.getElementById("next");
     const prevBtn = document.getElementById("prev");
     const pauseBtn = document.getElementById("pause");
@@ -47,6 +51,7 @@ const showAudios = (audios) => {
             audioName.innerText = audios[fileNumber].fileName;
             developer.innerText = audios[fileNumber].developer;
         }
+        playing.innerText = fileNumber + 1;
     })
 
     prevBtn.addEventListener("click", () => {
@@ -58,10 +63,11 @@ const showAudios = (audios) => {
         audioPath.src = audios[fileNumber].audioFile;
         audioName.innerText = audios[fileNumber].fileName;
         developer.innerText = audios[fileNumber].developer;
+        playing.innerText = fileNumber + 1;
     })
     pauseBtn.addEventListener("click", () => {
         const playPausebtn = document.getElementById("play-pause-btn")
-        const audioPaused = audioPath.paused
+        const audioPaused = audioPath.paused;
         if (audioPaused && playPausebtn.classList.contains("fa-caret-right")) {
             audioPath.play();
             playPausebtn.classList.add("fa-pause")
