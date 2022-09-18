9c4392a2de07f6d4797e5ccf8ec523c71cde56df
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-18 15:19:08 +0600

    Displayed all music on UI

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ const loadAudios = () => {
     const url  = "./audios.json";
     fetch(url)
     .then(res => res.json())
-        .then(data => showAudios(data.audios))
+        .then(data => { showAudios(data.audios), displayAudios(data.audios) })
 }
+
+// display all the audios to the UI as list item
+const displayAudios = (audios) => {
+    const totalMusic = document.getElementById("total-music");
+    totalMusic.innerText = audios.length;
+    const musicContainer = document.getElementById("music-list");
+    audios.forEach(audio => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <span>${audio.id}. </span> <span> ${audio.fileName.slice(0, 25)}</span>
+        `;
+        musicContainer.appendChild(li);
+
+    });
+}
+
 const showAudios = (audios) => {
     let fileNumber = 0;
     const nextBtn = document.getElementById("next");
